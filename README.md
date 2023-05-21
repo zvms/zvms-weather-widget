91d06956c44b89772870cb4957bfda11eda0a547
_Kerman <kermanx@qq.com>
2023-05-21 06:59:52 +0000

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+https://qweather.com
--- index.js
+++ index.js
@@ -4,13 +4,18 @@ import Koa from "koa";
 const app = new Koa();
 
 var catchedBody = null;
-
+/*
 app.listen(4001, () => {
   console.log("open server localhost:4001");
 });
-
+*/
 app.use(async (ctx) => {
   ctx.body = catchedBody ?? "Initializing, please refresh seconds later.";
+})
+
+const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Server started on localhost:${PORT} .`)
 })
 
 import Axios from "axios";
@@ -19,13 +24,19 @@ import KEY from "./private_key.js"
 
 const LOCATION = "121.714351,29.952756"
 
-
 async function updateBody() {
   try {
-    const url = `https://devapi.qweather.com/v7/grid-weather/7d?key=${KEY}&location=${LOCATION}`;
-    console.log("GET", url)
-    const response = await Axios.get(url);
-    if (response.status !== 200 || response.data.code !== "200") {
+    console.log(`\n\n\nupdating. [${Date()}]`);
+    const url24h = `https://devapi.qweather.com/v7/grid-weather/24h?key=${KEY}&location=${LOCATION}`;
+    const url7d = `https://devapi.qweather.com/v7/grid-weather/7d?key=${KEY}&location=${LOCATION}`;
+    console.log(url24h, url7d);
+    const response24h = await Axios.get(url24h);
+    const response7d = await Axios.get(url7d);
+    console.log("API called.");
+    const isErr24h = response24h.status !== 200 || response24h.data.code !== "200";
+    const isErr7d = response7d.status !== 200 || response7d.data.code !== "200"
+    if (isErr24h || isErr7d) {
+      const response = isErr24h ? response24h : response7d;
       throw Error(`
       <!DOCTYPE html>
       <h1>天气服务出错啦</h1>
@@ -35,24 +46,31 @@ async function updateBody() {
     `);
     }
 
-    const weatherDaily = response.data;
+    const weather24h = response24h.data;
+    const weather7d = response7d.data;
 
     let newBody = "";
-    // newBody += "<h1>当前天气（位置：镇海中学）</h1>";
-    // newBody += "<ul>";
-    // for (const { name, value } of response.data.results[0].now) {
-    //   newBody += `<li>${name}：${value}</li>`;
-    // }
-    // newBody += "</ul>";
     newBody += `<!DOCTYPE html>`
     newBody += "<h3>天气预报（位置：镇海中学）</h3>";
-    newBody += `数据更新时间：${weatherDaily.updateTime}`
+    newBody += `24hours数据更新于${new Date(weather24h.updateTime).toLocaleString()}<br/>`
+    newBody += `7days数据更新于${weather7d.updateTime}`
     newBody += "<ul>";
-    for (const { textDay, textNight, tempMax, tempMin, precip, phs, fxDate, wns } of weatherDaily.daily) {
+    for (const { fxTime, temp, text, precip } of weather24h.hourly) {
+      //const date=fxTime.slice(5,10);
+      //const hour=parseInt(fxTime.slice(11,13));
+      //const minute=fxTime.slice();
       newBody += `<li>`
-      newBody += `<strong>${fxDate}</strong><br/>`
+      newBody += `小时预报：<strong>${(new Date(fxTime)).toLocaleString()}</strong><br/>`
+      newBody += `气温：${temp}<br/>`
+      newBody += `天气状况：${text}<br/>`
+      newBody += `降水量：${precip}mm`
+      newBody += `</li>`
+    }
+    for (const { textDay, textNight, tempMax, tempMin, precip, fxDate } of weather7d.daily) {
+      newBody += `<li>`
+      newBody += `每日预报：<strong>${fxDate}</strong><br/>`
       newBody += `气温：${tempMin}℃ ~ ${tempMax}℃<br/>`
-      newBody += ` 天气状况：<ul><li>白天：${textDay ?? "<UNKNOWN>"}</li>`
+      newBody += `天气状况：<ul><li>白天：${textDay ?? "<UNKNOWN>"}</li>`
       newBody += `<li>夜晚：${textNight ?? "<UNKNOWN>"}</li></ul>`
       newBody += `降水：${precip}mm <br/>`
       //newBody += `风速：${wns}m/s<br/>`
@@ -65,6 +83,7 @@ async function updateBody() {
   } catch (e) {
     catchedBody = "ERROR: " + e;
   }
+  console.log("UPDATED:\n", catchedBody)
 }
 
 
